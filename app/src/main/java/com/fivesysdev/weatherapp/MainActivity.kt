4d78ce2ff352eb4feab09ef7be7b171ebabfc3b2
cyber-police <tsegolnykbogitsar@ukr.net>
2023-07-10 23:20:13 +0300

    UI creation

---
+++ app/src/main/java/com/fivesysdev/weatherapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fivesysdev.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // TO-DO dimens (margins)
+    }
+}
